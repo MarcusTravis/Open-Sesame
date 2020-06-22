03147b1c517a5a7c3f88474bc9c49030cda4ee2a
Marcus <mpt789@gmail.com>
2020-06-22 09:21:33 -0700

    added table and got app working again without handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,8 +11,8 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render("signup")
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.json("signup")
   });
 
   app.get("/login", function(req, res) {
